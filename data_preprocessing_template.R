fe99ba211de65678c8046915eff70fa1f23f23b2
Arron Stone <37765301+Abrarahmed786@users.noreply.github.com>
2019-08-30 20:03:00 +0530

    Update data_preprocessing_template.R

--- data_preprocessing_template.R
+++ data_preprocessing_template.R
@@ -1,5 +1,9 @@
+#importing libs
+
+#importing the dataset
 dataset = read.csv('Data.csv')
 
+#replacing empty values
 dataset$Age = ifelse(is.na(dataset$Age),
                      ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
                      dataset$Age)
@@ -8,9 +12,24 @@ dataset$Salary = ifelse(is.na(dataset$Salary),
                      ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
                      dataset$Salary)
 
+#encoding categorical data
 dataset$Country = factor(dataset$Country,
                          levels = c('France','Spain', 'Germany'),
                          labels = c(1,2,3))
+
 dataset$Purchased = factor(dataset$Purchased,
                          levels = c('No','Yes'),
                          labels = c(0,1))
+
+#spliting into test and train
+#import caTools
+
+#install.packages('caTools')
+
+library(caTools)
+
+set.seed(123)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
+
+training_set = subset (dataset, split == TRUE)
+test_set = subset (dataset, split == FALSE)
